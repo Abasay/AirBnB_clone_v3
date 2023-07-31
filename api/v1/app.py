309d8580fbc9f357359184f11a4e9283bf2cbfe3
Abasay <abdulsalamasheem@gmail.com>
2023-07-31 05:33:36 -0700

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """The flask app file"""
-
-
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
+from flask import Flask, jsonify, make_response
 from models import storage
+from os import getenv
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 
+
 @app.teardown_appcontext
 def close_storage(exception):
     """function that closes the storage session"""
     storage.close()
+
 
 @app.errorhandler(404)
 def not_found(error):
